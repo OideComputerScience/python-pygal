4994a6792a40a335ba5532fa7503a27eaec7e777
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 14:37:26 +0000

    Add files via upload

---
+++ ireland medals.py
@@ -0,0 +1,11 @@
+#Python Pygal PLE
+#Author: Carol Cronin
+#Contact: computerscience@oide.ie
+
+#The code displays gold, silver and bronze medals won by Ireland
+#at the 1992-2024 Summer Olympics
+
+years = ["1992","1996","2000","2004","2008","2012","2016","2020","2024"]
+gold = [1, 3, 0, 0, 0, 1, 0, 2, 4]
+silver = [1, 0, 1, 0, 1, 1, 2, 0, 0]
+bronze = [0, 1, 0, 0, 2, 3, 0, 2, 3]
