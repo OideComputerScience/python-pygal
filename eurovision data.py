4d1b616f2d77dd7ed884a99dcdec83d1d4d8a086
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 21:41:37 +0000

    Add files via upload

---
+++ eurovision data.py
@@ -0,0 +1,15 @@
+# Python Pygal PLE
+# Author: Carol Cronin
+# Contact: computerscience@oide.ie
+
+# Total wins by country
+countries = ["Ireland", "Sweden", "France", "Luxembourg", "Netherlands", "United Kingdom", "Israel"]
+total_wins = [7, 7, 5, 5, 5, 5, 4]
+
+# Nul points received
+nul_countries = ["Austria", "Norway", "Finland", "Spain", "Switzerland", "Germany"]
+nul_points = [4, 4, 3, 3, 3, 3]
+
+# Last-place finishes
+last_place_countries = ["Norway", "Finland", "Germany", "Austria"]
+last_place_finishes = [12, 9, 9, 7]
