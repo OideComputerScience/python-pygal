2a8cb424f1867cab0679339e5f6e381af68a39c3
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 14:21:59 +0000

    Add files via upload

---
+++ oct to dec temps.py
@@ -0,0 +1,11 @@
+#Python Pygal PLE
+#Author: Carol Cronin
+#Contact: computerscience@oide.ie
+
+#The code displays the average monthly temperature for Boyle, Roscommon and Castlerea
+#for the months October to December
+
+# Temperature data (°C)
+boyle_oct_dec = [9.9, 6.8, 5.0]
+roscommon_oct_dec = [10.5, 7.4, 5.6]
+castlerea_oct_dec = [10.1, 7.0, 5.2]
